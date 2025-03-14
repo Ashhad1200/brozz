dbe72fdaa1a6eea2cc7c1f739d4f5d56c545436f
Syed Ashhad <syedashad17@gmail.com>
2025-03-15 01:05:51 +0500

    FD

---
+++ react-shopping-cart/src/firebase/firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPd5a4ivE-JGhDWMbtg0X2GW2BPZJki18",
+  authDomain: "brozz-4c0b0.firebaseapp.com",
+  projectId: "brozz-4c0b0",
+  storageBucket: "brozz-4c0b0.firebasestorage.app",
+  messagingSenderId: "1004055652845",
+  appId: "1:1004055652845:web:c8430814e48c7e782f1fe4",
+  measurementId: "G-1PQ8J5GB3B"
+};
